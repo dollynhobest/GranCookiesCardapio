2c363ebde1099198ea4f979f9bf978906c61c163
dollynhobest <gabrielcaputti123@gmail.com>
2026-08-19 02:47:20 -0300

    Update cardapio.js

--- cardapio.js
+++ cardapio.js
@@ -16,8 +16,8 @@ const CARDAPIO = [
     nome: "Kinder Bueno",
     descricao: "Massa especial com chocolate branco e ao leite, recheado com creme de avelã branco artesanal e finalizada com pedaço de Kinder Bueno (aprox. 100g Massa e 30g Creme de Avelã)",
     preco: 18.50,
-    estoque: 10,
-    disponivel: false
+    estoque: 09,
+    disponivel: true
   },
 
   {
@@ -25,8 +25,8 @@ const CARDAPIO = [
     nome: "Grantella",
     descricao: "Massa aerada com chocolate ao leite e muita Nutella (Aprox. 30g Nutella, 90g Massa).",
     preco: 15.50,
-    estoque: 10,
-    disponivel: false
+    estoque: 09,
+    disponivel: true
   },
 
   {
@@ -34,8 +34,8 @@ const CARDAPIO = [
     nome: "Red Velvet",
     descricao: "Massa fofinha com chocolate branco recheada com geleia de morango e brigadeiro de cream cheese (aprox. 105g.",
     preco: 13.50,
-    estoque: 10,
-    disponivel: false
+    estoque: 09,
+    disponivel: true
   },
   
   {
@@ -43,8 +43,8 @@ const CARDAPIO = [
     nome: "KitKat",
     descricao: "Massa com chocolate blend Nestlé, recheado com muita pasta de KitKat e finalizada com um pedaço de KitKat (aprox. 130g, 90g de massa, 30g de Pasta Profissional, 10g de pedaços de KitKat)",
     preco: 15.50,
-    estoque: 10,
-    disponivel: false
+    estoque: 09,
+    disponivel: true
   },
 
   {
@@ -52,8 +52,8 @@ const CARDAPIO = [
     nome: "Prestígio",
     descricao: "Massa especial com chocolate em pó e cacau black, com chocolates blend e branco Nestlé, recheado com uma ganache artesanal de coco (aprox. 110g, 90g de massa especial e 20g de ganache)",
     preco: 14.50,
-    estoque: 10,
-    disponivel: false
+    estoque: 09,
+    disponivel: true
   },
 
   {
@@ -61,8 +61,8 @@ const CARDAPIO = [
     nome: "S'mores",
     descricao: "Massa amanteigada com chocolate ao leite, biscoito amanteigado e marshmallow artesanal (aprox. 120g)",
     preco: 14.50,
-    estoque: 10,
-    disponivel: false
+    estoque: 09,
+    disponivel: true
   },
 
   {
@@ -70,8 +70,8 @@ const CARDAPIO = [
     nome: "Alpino Black",
     descricao: "Massa especial de chocolate em pó e cacau black com pedaços de chocolate Alpino, recheada com Blend de chocolates nobre branco e ao leite (aprox. 120g, 90g de massa, 30g de chocolates branco e ao leite)",
     preco: 15.50,
-    estoque: 10,
-    disponivel: false
+    estoque: 09,
+    disponivel: true
   },
 
   {
@@ -79,8 +79,8 @@ const CARDAPIO = [
     nome: "Paçoca com Nutella",
     descricao: "Massa com chocolate ao leite e recheio de paçoca com nutella (aprox. 120g, 90g de massa, 30g de paçoca com nutella)",
     preco: 15.50,
-    estoque: 10,
-    disponivel: false
+    estoque: 09,
+    disponivel: true
   },
 
   {
@@ -88,8 +88,8 @@ const CARDAPIO = [
     nome: "Cappuccino",
     descricao: "Massa especial amanteigada com café Nestle Gold N°6 e chocolates meio amargo, ao leite e branco  (aprox. 120g)",
     preco: 14.50,
-    estoque: 10,
-    disponivel: false
+    estoque: 09,
+    disponivel: true
   },
 
   {
@@ -97,8 +97,8 @@ const CARDAPIO = [
     nome: "Churros",
     descricao: "Massa especial com chocolate branco e canela, recheada com doce de leite (aprox. 20g de doce de leite, 90g massa)",
     preco: 13.50,
-    estoque: 10,
-    disponivel: false
+    estoque: 09,
+    disponivel: true
   },
 
   {
@@ -106,8 +106,8 @@ const CARDAPIO = [
     nome: "Pistache",
     descricao: "Massa com chocolate branco e grãos de pistache, recheada com ganache artesanal de pistache (aprox. 20g de ganache, 90g de massa)",
     preco: 17.50,
-    estoque: 10,
-    disponivel: false
+    estoque: 09,
+    disponivel: true
   },
 
   {
@@ -115,7 +115,7 @@ const CARDAPIO = [
     nome: "Maracujá",
     descricao: "Massa com chocolate branco e geleia de maracujá, recheado com mais geleia e brigadeiro de maracujá (aprox. 120g)",
     preco: 13.50,
-    estoque: 10,
+    estoque: 09,
     disponivel: true
   },
  
@@ -124,7 +124,7 @@ const CARDAPIO = [
     nome: "Granfetti",
     descricao: "Massa aerada com muito chocolate ao leite e granulados coloridos (aprox. 120g)",
     preco: 10.50,
-    estoque: 10,
+    estoque: 09,
     disponivel: true
   },
 
@@ -151,7 +151,7 @@ const CARDAPIO = [
     nome: "Chocochip Milka",
     descricao: "Massa amanteigada com muito chocolate Milka ao leite (aprox. 45g chocolate Milka ao leite, 65g massa amanteigada)",
     preco: 20.50,
-    estoque: 10,
+    estoque: 09,
     disponivel: true
   },
     
@@ -160,8 +160,8 @@ const CARDAPIO = [
     nome: "Oreo",
     descricao: "Massa com chocolate nobre ao leite e pedaços de bolacha Oreo, recheado com uma ganache de baunilha e chocolate branco (aprox. 110g)",
     preco: 14.50,
-    estoque: 10,
-    disponivel: false
+    estoque: 09,
+    disponivel: true
   },
     
   {
@@ -169,8 +169,8 @@ const CARDAPIO = [
     nome: "Bacon",
     descricao: "Massa amanteigada com chocolates branco e ao leite e pedaços de bacon gourmet (aprox. 105g)",
     preco: 13.50,
-    estoque: 10,
-    disponivel: false
+    estoque: 09,
+    disponivel: true
   },
     
   {
@@ -178,8 +178,8 @@ const CARDAPIO = [
     nome: "Caramel Lotus",
     descricao: "Massa especial com manteiga clarificada e com chocolate branco, recheado com creme de Biscoito Lotus e ganache de caramelo e finalizado com um biscoito Lotus (Aprox. 90g de massa, 10g de creme de Biscoito Lotus, 10g de ganache de caramelo)",
     preco: 15.50,
-    estoque: 10,
-    disponivel: false
+    estoque: 09,
+    disponivel: true
  },
     
   // Exemplo para adicionar outro:
