19172fbf0a926af30ee70046504e035eb8215091
dollynhobest <gabrielcaputti123@gmail.com>
2026-08-19 03:54:09 -0300

    Update cardapio.js

--- cardapio.js
+++ cardapio.js
@@ -25,7 +25,7 @@ const CARDAPIO = [
     nome: "Grantella",
     descricao: "Massa aerada com chocolate ao leite e muita Nutella (Aprox. 30g Nutella, 90g Massa).",
     preco: 15.50,
-    estoque: 09,
+    estoque: 01,
     disponivel: true
   },
 
@@ -44,7 +44,7 @@ const CARDAPIO = [
     descricao: "Massa com chocolate blend Nestlé, recheado com muita pasta de KitKat e finalizada com um pedaço de KitKat (aprox. 130g, 90g de massa, 30g de Pasta Profissional, 10g de pedaços de KitKat)",
     preco: 15.50,
     estoque: 09,
-    disponivel: true
+    disponivel: false
   },
 
   {
@@ -53,7 +53,7 @@ const CARDAPIO = [
     descricao: "Massa especial com chocolate em pó e cacau black, com chocolates blend e branco Nestlé, recheado com uma ganache artesanal de coco (aprox. 110g, 90g de massa especial e 20g de ganache)",
     preco: 14.50,
     estoque: 09,
-    disponivel: true
+    disponivel: false
   },
 
   {
@@ -62,7 +62,7 @@ const CARDAPIO = [
     descricao: "Massa amanteigada com chocolate ao leite, biscoito amanteigado e marshmallow artesanal (aprox. 120g)",
     preco: 14.50,
     estoque: 09,
-    disponivel: true
+    disponivel: false
   },
 
   {
@@ -80,7 +80,7 @@ const CARDAPIO = [
     descricao: "Massa com chocolate ao leite e recheio de paçoca com nutella (aprox. 120g, 90g de massa, 30g de paçoca com nutella)",
     preco: 15.50,
     estoque: 09,
-    disponivel: true
+    disponivel: false
   },
 
   {
@@ -98,7 +98,7 @@ const CARDAPIO = [
     descricao: "Massa especial com chocolate branco e canela, recheada com doce de leite (aprox. 20g de doce de leite, 90g massa)",
     preco: 13.50,
     estoque: 09,
-    disponivel: true
+    disponivel: false
   },
 
   {
@@ -107,7 +107,7 @@ const CARDAPIO = [
     descricao: "Massa com chocolate branco e grãos de pistache, recheada com ganache artesanal de pistache (aprox. 20g de ganache, 90g de massa)",
     preco: 17.50,
     estoque: 09,
-    disponivel: true
+    disponivel: false
   },
 
   {
@@ -116,7 +116,7 @@ const CARDAPIO = [
     descricao: "Massa com chocolate branco e geleia de maracujá, recheado com mais geleia e brigadeiro de maracujá (aprox. 120g)",
     preco: 13.50,
     estoque: 09,
-    disponivel: true
+    disponivel: false
   },
  
   {
@@ -125,7 +125,7 @@ const CARDAPIO = [
     descricao: "Massa aerada com muito chocolate ao leite e granulados coloridos (aprox. 120g)",
     preco: 10.50,
     estoque: 09,
-    disponivel: true
+    disponivel: false
   },
 
   {
@@ -134,7 +134,7 @@ const CARDAPIO = [
     descricao: "Massa aerada com chocolate ao leite e muito Ovomaltine Cremoso (Aprox. 30g Ovomaltien Cremoso, 90g Massa)",
     preco: 15.50,
     estoque: 09,
-    disponivel: true
+    disponivel: false
   },
     
   {
@@ -143,7 +143,7 @@ const CARDAPIO = [
     descricao: "Massa com chocolate branco e chocolate ao leite, recheada com brigadeiro artesanal de Ninho e Nutella, decorado com coberturas de chocolate branco e blend (aprox. 15g de Brigadeiro de Ninho e 15g de Nutella, 90g de Massa; Decoração com aprox. 15g de Coberturas)",
     preco: 16.50,
     estoque: 09,
-    disponivel: true
+    disponivel: false
   },
     
   {
@@ -152,7 +152,7 @@ const CARDAPIO = [
     descricao: "Massa amanteigada com muito chocolate Milka ao leite (aprox. 45g chocolate Milka ao leite, 65g massa amanteigada)",
     preco: 20.50,
     estoque: 09,
-    disponivel: true
+    disponivel: false
   },
     
   {
@@ -160,7 +160,7 @@ const CARDAPIO = [
     nome: "Oreo",
     descricao: "Massa com chocolate nobre ao leite e pedaços de bolacha Oreo, recheado com uma ganache de baunilha e chocolate branco (aprox. 110g)",
     preco: 14.50,
-    estoque: 09,
+    estoque: 01,
     disponivel: true
   },
     
@@ -170,7 +170,7 @@ const CARDAPIO = [
     descricao: "Massa amanteigada com chocolates branco e ao leite e pedaços de bacon gourmet (aprox. 105g)",
     preco: 13.50,
     estoque: 09,
-    disponivel: true
+    disponivel: false
   },
     
   {
