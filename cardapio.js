4acac9a2e3b69956f142c7edb30f9f974ab5b70c
dollynhobest <gabrielcaputti123@gmail.com>
2026-08-19 15:46:12 -0300

    Update cardapio.js

--- cardapio.js
+++ cardapio.js
@@ -71,7 +71,7 @@ const CARDAPIO = [
     descricao: "Massa especial de chocolate em pó e cacau black com pedaços de chocolate Alpino, recheada com Blend de chocolates nobre branco e ao leite (aprox. 120g, 90g de massa, 30g de chocolates branco e ao leite)",
     preco: 15.50,
     estoque: 09,
-    disponivel: true
+    disponivel: false
   },
 
   {
